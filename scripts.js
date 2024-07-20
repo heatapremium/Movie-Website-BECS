c7594c47195cc2e87f2685b98f8f6f556c7dd85d
Himal_geethanjana <himalgeethanjana18@gmail.com>
2024-07-20 14:09:38 +0530

    Home Page

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+// Placeholder script file
+document.querySelector('.search-bar input').addEventListener('input', function() {
+    alert('Search functionality to be implemented.');
+});
+
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('Start watching functionality to be implemented.');
+    });
+});
